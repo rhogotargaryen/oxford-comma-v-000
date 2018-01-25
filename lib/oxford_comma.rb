51ff6dfd39a6a36c45ebc5077f3985fa03dc3356
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 06:18:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-  last_item = array.last
-  array.pop
-  return "#{array.join(" , ")}and #{last_item}"
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    return "#{array.join(" , ")}and #{last_item}."
+
+
 end
