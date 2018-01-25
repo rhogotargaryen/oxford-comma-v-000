c50e8d42336719a2c5a2a684a688d485afc303be
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 06:19:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    return "#{array.join(", ")}, and #{last_item}"
+  end
 end
